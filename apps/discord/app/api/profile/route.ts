134852fcfb47768411554d4edaeb68ccaab30afc
sarthakroy107 <sarthakroy107@gmail.com>
2024-04-16 23:03:27 +0530

    date-16-april-2024

--- apps/discord/app/api/profile/route.ts
+++ apps/discord/app/api/profile/route.ts
@@ -2,6 +2,7 @@ import { currentProfile } from "@/lib/auth/current-user";
 import { NextRequest, NextResponse } from "next/server";
 import type { TProfileDTO } from "@db/dto/profile/profile-body-dto";
 export type TAPIProfile = TProfileDTO;
+
 export async function GET(req: NextRequest) {
   const profile = await currentProfile();
   if (profile.status !== 200 || profile.data === null) {
